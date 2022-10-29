068de92cc7f9bb85fdddf6e5cc5e35015cbc2caa
Pedro Alves <pta2002@pta2002.com>
2022-10-29 20:58:30 +0100

    Asynchronously fetch games

--- lib/rasbet_web/live/game_live/index.ex
+++ lib/rasbet_web/live/game_live/index.ex
@@ -3,8 +3,19 @@ defmodule RasbetWeb.GameLive.Index do
 
   alias Rasbet.Game.TwoTeams
 
+  def handle_info({:update_games}, socket) do
+    case TwoTeams.Api.games() do
+      {:ok, games} -> {:noreply, socket |> assign(:games, games)}
+      {:error, _} -> {:noreply, socket |> assign(:games, "Erro ao carregar jogos")}
+    end
+  end
+
   def mount(_params, _session, socket) do
-    {:ok, games} = TwoTeams.Api.games()
-    {:ok, socket |> assign(:games, games)}
+    # {:ok, games} = TwoTeams.Api.games()
+    if connected?(socket) do
+      send(self(), {:update_games})
+    end
+
+    {:ok, socket |> assign(:games, nil)}
   end
 end
