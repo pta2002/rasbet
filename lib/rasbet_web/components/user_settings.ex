27701227412abc3d711bfacc8edaf251e0ad70ad
Mariii-23 <marianadinis8@hotmail.com>
2022-12-04 11:39:49 +0000

    Fix space between butons -> settings page

--- lib/rasbet_web/components/user_settings.ex
+++ lib/rasbet_web/components/user_settings.ex
@@ -121,10 +121,8 @@ defmodule RasbetWeb.Components.UserSettings do
 
         <div class="flex place-content-between w-full items-center flex-row-reverse">
           <%= if @edit do %>
-            <div class="flex w-full items-center justify-center">
               <%= submit gettext("Confirmar"), class: "bg-primary-500 px-6 py-2 rounded-full text-white w-80" %>
-            </div>
-            <%= link "Apagar Conta", to: Routes.user_session_path(@socket,:delete), method: :delete, phx_click: "delete",  data: [confirm: "Are you sure?"], class: "text-red-600 w-full w-80"%>
+              <%= link "Apagar Conta", to: Routes.user_session_path(@socket,:delete), method: :delete, phx_click: "delete",  data: [confirm: "Are you sure?"], class: "text-red-600 w-full"%>
           <% else %>
             <%= submit gettext("Registar"), class: "bg-primary-500 px-6 py-2 rounded-full text-white" %>
             <div><%= gettext "Já tem conta?"%> <a href="/users/log_in" class="text-primary-500 font-semibold"><%= gettext "Entrar"%></a></div>
